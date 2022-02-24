5e2d1fe481580d58ed7b26e4a5c8200fe4ac7c84
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-02-24 17:24:12 +0100

    Added nav and placeholder donation

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<body>
+    <nav>
+        <h1>Batnana</h1>
+        <img src="/favicon.svg" alt="">
+    </nav>
+    <slot></slot>
+</body>
+
+<style>
+    * {
+		margin: 0;
+		font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+	}
+
+	nav {
+        background-color: black;
+        color: #F8B323;
+        font-size: 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+        padding: .5rem;
+	}
+
+	img {
+		width: 4rem;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <p>2389472893 €</p>
+</main>
+
+<style>
+</style>
